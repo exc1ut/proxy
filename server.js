1004147242d188517d8ba7822992b6038ed9253b
exc1ut <umid19999@gmail.com>
2021-06-14 22:06:48 +0500

    some name

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
-var http = require('http');
-const port = process.env.PORT || 3000;
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-http.createServer(onRequest).listen(port);
+const app = express();
 
-function onRequest(client_req, client_res) {
-  console.log('serve: ' + client_req.url);
-
-  var options = {
-    hostname: 'www.youtube.com',
-    port: 80,
-    path: client_req.url,
-    method: client_req.method,
-    headers: client_req.headers
-  };
-
-  var proxy = http.request(options, function (res) {
-    client_res.writeHead(res.statusCode, res.headers)
-    res.pipe(client_res, {
-      end: true
-    });
-  });
-
-  client_req.pipe(proxy, {
-    end: true
-  });
-}
+app.use('/api', createProxyMiddleware({ target: 'https://www.youtube.com/', changeOrigin: false }));
+app.listen(3000);
